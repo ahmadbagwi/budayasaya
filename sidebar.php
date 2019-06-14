846d85cac99f5c41ffb2c4b33499363a753b86d9
ahmadbagwi <ahmadbagwi.id@gmail.com>
2019-06-14 14:26:40 +0700

    fix col sidebar

--- sidebar.php
+++ sidebar.php
@@ -11,7 +11,7 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
-			<div class="col-lg-4 col-sm-12 col-xs-12">
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
 				<aside id="secondary" class="widget-area">
 					<?php dynamic_sidebar( 'sidebar-1' ); ?>
 				</aside><!-- #secondary -->
