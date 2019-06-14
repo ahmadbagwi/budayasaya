6193e5d8bfe5a8ac5c5fdcddc8d0d2481d61b1ef
ahmadbagwi <ahmadbagwi.id@gmail.com>
2019-06-14 14:20:32 +0700

    remove text copyright

--- footer.php
+++ footer.php
@@ -46,27 +46,6 @@
 
 		</div>
 	</div>
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12 col-sm-12 col-xs-12">
-				<footer id="colophon" class="site-footer">
-					<div class="site-info">
-						<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'budayasaya' ) ); ?>">
-							<?php
-							/* translators: %s: CMS name, i.e. WordPress. */
-							printf( esc_html__( 'Proudly powered by %s', 'budayasaya' ), 'WordPress' );
-							?>
-						</a>
-						<span class="sep"> | </span>
-						<?php
-						/* translators: 1: Theme name, 2: Theme author. */
-						printf( esc_html__( 'Theme: %1$s by %2$s.', 'budayasaya' ), 'budayasaya', '<a href="https://ahmadbagwi.id">Ahmad Bagwi</a>' );
-						?>
-					</div><!-- .site-info -->
-				</footer><!-- #colophon -->
-			</div>
-		</div>
-	</div>		
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
