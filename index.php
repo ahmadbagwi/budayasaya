0116304766542060d9ff14859ca121acf4fa9589
ahmadbagwi <ahmadbagwi.id@gmail.com>
2019-06-18 16:11:00 +0700

    fix layout default index or blog post as 2 col

--- index.php
+++ index.php
@@ -16,10 +16,10 @@ get_header();
 ?>
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-12 col-sm-12 col-xs-12">
+			<div class="col-lg-8 col-md-8 col-sm-12">
 				<div id="primary" class="content-area">
 					<main id="main" class="site-main">
-
+					<div class="row">
 					<?php
 					if ( have_posts() ) :
 
@@ -31,7 +31,9 @@ get_header();
 						<?php
 					endif;
 
-					/* Start the Loop */
+					/* Start the Loop */?>
+					
+					<?php
 					while ( have_posts() ) :
 						the_post();
 
@@ -40,10 +42,10 @@ get_header();
 								 * If you want to override this in a child theme, then include a file
 								 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 								 */
-								get_template_part( 'template-parts/content', get_post_type() );
+								get_template_part( 'template-parts/content-archive', get_post_type() );
 
-							endwhile;
-
+							endwhile; ?>
+							</div><?php
 							the_posts_navigation();
 
 						else :
@@ -56,8 +58,9 @@ get_header();
 					</main><!-- #main -->
 				</div><!-- #primary -->
 			</div>
-		</div>
-	</div>
+		<!-- </div> -->
+	<!-- </div> -->
 
 <?php
+get_sidebar();
 get_footer();
